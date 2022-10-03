568fa95bbd4b93f8fbf1bd6849ba7def479b9698
gbstorey <gbstorey@asu.edu>
2022-10-03 16:53:33 -0700

    Include floating effect on appbar while scrolling

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import { useScrollTrigger } from '@mui/material';
+
+function ElevationScroll(props) {
+    const { children } = props;
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0,
+    });
+  
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
 
 export default function Header(props) {
     return (
-        <AppBar position="fixed">
-            <Toolbar>
-                Arc Development
-            </Toolbar>
-        </AppBar>
+        <ElevationScroll {...props}>
+            <AppBar position="fixed">
+                <Toolbar>
+                    Arc Development
+                </Toolbar>
+            </AppBar>
+        </ElevationScroll>
     );
 }
